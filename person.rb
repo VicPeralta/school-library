52a758932b4f43e10c8e69e48e95238d30150fd8
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-05-08 13:20:41 -0500

    Change method name is_of_age? to of_age? to avoid linter error
    modified:   person.rb

--- person.rb
+++ person.rb
@@ -9,17 +9,16 @@ class Person
   # getters
   attr_reader :id
 
+  # setters
   attr_accessor :name, :age
 
-  # setters
-
   def can_use_services?
-    @age >= 18 || parent_permission
+    of_age? || parent_permission
   end
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
