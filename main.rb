de1eb557bd737147b99b7dfa5cef9bb5f620be12
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-05-09 17:43:31 -0500

    Add relationship Person- > Book -> Rental
    modified:   main.rb
    modified:   person.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require './person'
 require './student'
 require './teacher'
+require './book'
 require './capitalize_decorator'
 require './trimmer_decorator'
+require './classroom'
 
 person = Person.new(50, 'maximilianus')
 puts "Person id:#{person.id}"
@@ -13,3 +15,12 @@ capitalized_trimmer_person = TrimmerDecorator.new(capitalized_person)
 puts "Person name capitalized and trimmed: #{capitalized_trimmer_person.correct_name}"
 capitalized_trimmer_person.nameable = person
 puts "Person name just trimmed: #{capitalized_trimmer_person.correct_name}"
+
+classroom= Classroom.new('102')
+classroom2= Classroom.new('202')
+student = Student.new(50,classroom, 'Victor')
+
+book = Book.new('C++ Programming', 'Victor Peralta')
+Rental.new('2022/05/09', student, book )
+print student.rentals
+print book.rentals
