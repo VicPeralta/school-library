bf8ed5d710e0c4c4a4ce11faa0a589a536cdfdf6
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-05-09 17:42:04 -0500

    Add relationships between student and classroom
    modified:   student.rb

--- student.rb
+++ student.rb
@@ -3,7 +3,14 @@ require './person'
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     @classroom = classroom
+    # classroom.students.push(self) unless classroom.students.include?(self)
     super(age, name, parent_permission: parent_permission)
+  end
+  attr_reader  :classroom
+  
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
