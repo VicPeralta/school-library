79ac736b62f9c661fbcbc1a7cfc70f0366f1accc
andres-condezo <ra.condezomonge@gmail.com>
2022-05-16 14:59:43 -0500

    Create terminal_output function to show messages in console

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require './valid_date'
 require './library'
 require './menu'
+require './terminal_output'
 
 # This class is responsible to ask and validate user's information
 
@@ -11,29 +12,29 @@ class App
   end
 
   def select_book_from_list
-    puts 'Select a book from the following list by number'
+    terminal_puts 'Select a book from the following list by number'
     @library.books.each_with_index do |book, index|
-      puts "#{index}) Title: \"#{book.title}\" Author: #{book.author} "
+      terminal_puts "#{index}) Title: \"#{book.title}\" Author: #{book.author} "
     end
     gets.chomp.to_i
   end
 
   def select_person_from_list
-    puts 'Select a person from the following list by number (not id)'
+    terminal_puts 'Select a person from the following list by number (not id)'
     @library.people.each_with_index do |person, index|
-      print "#{index}) #{person.is_a?(Teacher) ? '[Teacher]' : '[Student]'} "
-      puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      terminal_print "#{index}) #{person.is_a?(Teacher) ? '[Teacher]' : '[Student]'} "
+      terminal_puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
     gets.chomp.to_i
   end
 
   def can_create_rental?
     if @library.books.empty?
-      puts 'There are no books for rentals'
+      terminal_puts 'There are no books for rentals'
       return false
     end
     if @library.people.empty?
-      puts 'There are no people for rentals'
+      terminal_puts 'There are no people for rentals'
       return false
     end
     true
@@ -41,37 +42,37 @@ class App
 
   def list_rentals
     if @library.rentals.empty?
-      puts 'No Rentals to show'
+      terminal_puts 'No Rentals to show'
       return
     end
-    print 'ID of person: '
+    terminal_print 'ID of person: '
     id = gets.chomp
-    puts 'Rentals'
+    terminal_puts 'Rentals'
     @library.rentals.each do |rental|
       if rental.person.id == id.to_i
-        print("Date: #{rental.date} ")
-        puts("Book \"#{rental.book.title}\" by #{rental.book.author} ")
+        terminal_print("Date: #{rental.date} ")
+        terminal_puts("Book \"#{rental.book.title}\" by #{rental.book.author} ")
       end
     end
   end
 
   def list_books
     if @library.books.empty?
-      puts 'There are no books to show'
+      terminal_puts 'There are no books to show'
       return
     end
-    @library.books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    @library.books.each { |book| terminal_puts "Title: \"#{book.title}\", Author: #{book.author}" }
   end
 
   def list_people
     if @library.people.empty?
-      puts 'There are no people to show'
+      terminal_puts 'There are no people to show'
       return
     end
     @library.people.each do |person|
-      print '[Teacher] ' if person.is_a?(Teacher)
-      print '[Student] ' if person.is_a?(Student)
-      puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      terminal_print '[Teacher] ' if person.is_a?(Teacher)
+      terminal_print '[Student] ' if person.is_a?(Student)
+      terminal_puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
@@ -80,35 +81,35 @@ class App
 
     index_book = select_book_from_list
     if index_book.negative? || index_book >= @library.books.length
-      puts 'Invalid selection'
+      terminal_puts 'Invalid selection'
       return
     end
     index_person = select_person_from_list
     if index_person.negative? || index_person >= @library.people.length
-      puts 'Invalid selection'
+      terminal_puts 'Invalid selection'
       return
     end
-    puts 'Date (yyyy/mm/dd):'
+    terminal_puts 'Date (yyyy/mm/dd):'
     date = gets.chomp
     if valid_date(date)
       @library.add_rental(date, index_person, index_book)
-      puts 'Rental created successfully'
+      terminal_puts 'Rental created successfully'
       return
     end
-    puts 'Invalid date'
+    terminal_puts 'Invalid date'
   end
 
   def create_book
-    print 'Title: '
+    terminal_print 'Title: '
     title = gets.chomp
-    print 'Author: '
+    terminal_print 'Author: '
     author = gets.chomp
     @library.add_book(title, author)
-    puts 'Book created successfully'
+    terminal_puts 'Book created successfully'
   end
 
   def new_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    terminal_print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
     option = gets.chomp.to_i
     return if option < 1 && option > 2
 
@@ -116,30 +117,30 @@ class App
     when 1
       age, name, permission = student_info
       @library.create_student(age, name, parent_permission: permission)
-      puts 'Person created successfully'
+      terminal_puts 'Person created successfully'
     when 2
       age, name, specialization = teacher_info
       @library.create_teacher(age, specialization, name)
-      puts 'Person created successfully'
+      terminal_puts 'Person created successfully'
     end
   end
 
   def student_info
-    print 'Age: '
+    terminal_print 'Age: '
     age = gets.chomp
-    print 'Name: '
+    terminal_print 'Name: '
     name = gets.chomp
-    print 'Has parent permission? [Y/N]: '
+    terminal_print 'Has parent permission? [Y/N]: '
     permission = gets.chomp
     [age, name, permission.downcase == 'y']
   end
 
   def teacher_info
-    print 'Age: '
+    terminal_print 'Age: '
     age = gets.chomp
-    print 'Name: '
+    terminal_print 'Name: '
     name = gets.chomp
-    print 'Specialization: '
+    terminal_print 'Specialization: '
     specialization = gets.chomp
     [age, name, specialization]
   end
