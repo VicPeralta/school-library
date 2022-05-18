326230dde87f99e80c375edbb38d3f5a21c91003
andres-condezo <ra.condezomonge@gmail.com>
2022-05-18 12:07:30 -0500

    Create tests for person and student classes

---
+++ spec/student_spec.rb
@@ -0,0 +1,29 @@
+require './person'
+require './student'
+require './classroom'
+require 'json'
+
+RSpec.describe Person do
+  before :each do
+    @student = Student.new(10, Classroom.new('1B'), 'Omar')
+  end
+
+  it 'Create an instance of person class' do
+    expect(@student).to be_a_kind_of(Person)
+  end
+
+  it 'Create an instance of student class' do
+    expect(@student).to be_an_instance_of(Student)
+  end
+
+  it 'Check the classroom label' do
+    expect(@student.classroom.label).to eq('1B')
+  end
+
+  it 'create JSON string' do
+    @student = Student.new('10', Classroom.new('1B'), 'Omar', 53, parent_permission: true)
+    json = JSON.generate(@student)
+    json = @student.to_json
+    expect(json).to eq '{"json_class":"Student","data":["10",{"json_class":"Classroom","label":"1B"},"Omar",53,{"parent_permission":true}]}'
+  end
+end
