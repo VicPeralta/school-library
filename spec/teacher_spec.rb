38c5380be7dfa40453e5208f348a7ec5f12a5968
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-18 23:36:47 +0430

    Add tests for classroom class

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -4,7 +4,7 @@ require './teacher'
 require './capitalize_decorator'
 require 'json'
 
-RSpec.describe Person do
+RSpec.describe Teacher do
   teacher = Teacher.new('45', 'Math', 'behnam', 645)
   it 'The teacher instance should be a person ' do
     expect(teacher).to be_a_kind_of(Person)
@@ -23,7 +23,7 @@ RSpec.describe Person do
   end
 
   it 'Converting a json string to an instance of Teacher class' do
-    teacher = JSON.parse('{"json_class":"Teacher","data":["45","Math","behnam",645]}' , create_additions: true)
+    teacher = JSON.parse('{"json_class":"Teacher","data":["45","Math","behnam",645]}', create_additions: true)
     expect(teacher).to be_an_instance_of(Teacher)
   end
 
@@ -33,6 +33,6 @@ RSpec.describe Person do
 
   it 'Using de3corators teacher name should be able to be capitalized' do
     capitalize_decorator = CapitalizeDecorator.new(teacher)
-    expect(capitalize_decorator.correct_name).to be == "Behnam"
+    expect(capitalize_decorator.correct_name).to be == 'Behnam'
   end
 end
